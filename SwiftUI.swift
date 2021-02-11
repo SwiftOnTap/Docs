d4c621992f89907ea3c356546158d7288b42f5d4
Aaron <aaron.moss@mac.com>
2021-02-11 10:16:59 -0800

    Add documentation for backgroundPreferenceValue

--- SwiftUI.swift
+++ SwiftUI.swift
@@ -29824,8 +29824,40 @@ extension View {
     @inlinable public func overlayPreferenceValue<Key, T>(_ key: Key.Type = Key.self, @ViewBuilder _ transform: @escaping (Key.Value) -> T) -> some View where Key : PreferenceKey, T : View { }
 
 
-    /// Uses the specified preference value from the view to produce another
-    /// view as a background to the first view.
+    /// Places a new view in the background of the first view, using the values from
+    /// the preferences.
+    ///
+    /// This creates a new view, placed in the background, using the view preferences from the parent.
+    /// This will not affect the layout of other views. Using backgroundPreferenceValue
+    /// is one of 3 ways for responding to view preferences. For information on
+    /// the other ways, see:
+    /// - ``View/overlayPreferenceValue(_:,transform:)``
+    /// - ``View/onPreferenceChange(_:,transform:)``
+    ///
+    /// For example:
+    ///
+    /// ![Background preference value example 1][backgroundpreference-example-1.png]
+    ///
+    /// ```
+    /// struct ExampleView: View {
+    ///
+    ///    var body: some View {
+    ///        Text("Bullseye")
+    ///            .preference(key: CustomPreferenceKey.self, value: Color.red)
+    ///            .backgroundPreferenceValue(CustomPreferenceKey.self) { PreferenceKey in
+    ///                Circle().fill(PreferenceKey)
+    ///        }
+    ///    }
+    /// }
+    ///
+    /// struct CustomPreferenceKey: PreferenceKey {
+    ///    static var defaultValue: Color = Color.clear
+    ///
+    ///    static func reduce(value: inout Color, nextValue: () -> Color) {
+    ///        value = nextValue()
+    ///    }
+    /// }
+    /// ```
     @inlinable public func backgroundPreferenceValue<Key, T>(_ key: Key.Type = Key.self, @ViewBuilder _ transform: @escaping (Key.Value) -> T) -> some View where Key : PreferenceKey, T : View { }
 
 }
