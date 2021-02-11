0ad27bf88b823bdd3757afd46ff5438e37ee9be9
Aaron <aaron.moss@mac.com>
2021-02-10 19:56:27 -0800

    Docs for ViewModifier

--- SwiftUI.swift
+++ SwiftUI.swift
@@ -31256,25 +31256,40 @@ extension ViewDimensions : Equatable {
 }
 
 /// A modifier that you apply to a view or another view modifier, producing a
-/// different version of the original value.
+/// new view.
 ///
 /// Adopt the `ViewModifier` protocol when you want to create a reusable
 /// modifier that you can apply to any view. The example below combines several
 /// modifiers to create a new modifier that you can use to create blue caption
 /// text surrounded by a rounded rectangle:
 ///
-///     struct BorderedCaption: ViewModifier {
-///         func body(content: Content) -> some View {{}
-///             content
-///                 .font(.caption2)
-///                 .padding(10)
-///                 .overlay(
-///                     RoundedRectangle(cornerRadius: 15)
-///                         .stroke(lineWidth: 1)
-///                 )
-///                 .foregroundColor(Color.blue)
-///         }
-///     }
+/// ![View modifier example 1][view-modifier-example-1.png]
+///
+/// ```
+/// struct ExampleView: View {
+///
+///    var body: some View {
+///        VStack {
+///            Text("Text without blue border")
+///            Text("Text with blue border")
+///                .modifier(BorderedCaption())
+///        }
+///    }
+/// }
+///
+/// struct BorderedCaption: ViewModifier {
+///    func body(content: Content) -> some View {
+///        content
+///            .font(.caption2)
+///            .padding(10)
+///            .overlay(
+///                RoundedRectangle(cornerRadius: 15)
+///                    .stroke(lineWidth: 1)
+///            )
+///            .foregroundColor(Color.blue)
+///    }
+/// }
+/// ```
 ///
 /// You can apply `modifier(_:)` directly to a view, but a more common and
 /// idiomatic approach uses `modifier(_:)` to define an extension to
@@ -31308,7 +31323,21 @@ extension ViewModifier {
     /// Gets the current body of the caller.
     ///
     /// `content` is a proxy for the view that will have the modifier
-    /// represented by `Self` applied to it.
+    /// represented by `Self` applied to it. For example:
+    /// ```
+    /// struct BorderedCaption: ViewModifier {
+    ///    func body(content: Content) -> some View {
+    ///        content
+    ///            .font(.caption2)
+    ///            .padding(10)
+    ///            .overlay(
+    ///                RoundedRectangle(cornerRadius: 15)
+    ///                    .stroke(lineWidth: 1)
+    ///            )
+    ///            .foregroundColor(Color.blue)
+    ///    }
+    /// }
+    /// ```
     func body(content: Self.Content) -> Self.Body { }
 
     /// The content view type passed to `body()`.
