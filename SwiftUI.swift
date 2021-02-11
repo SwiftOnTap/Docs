b9b44bd6ddfa49ba2b43c84e19894ca58f6f26fa
Aaron <aaron.moss@mac.com>
2021-02-10 17:06:06 -0800

    Document commands

--- SwiftUI.swift
+++ SwiftUI.swift
@@ -4005,6 +4005,15 @@ public struct CommandMenu<Content> : Commands where Content : View {
 
 /// Conforming types represent a group of related commands that can be exposed
 /// to the user via the main menu on macOS and key commands on iOS.
+///
+/// The available Command types are:
+/// - ``CommandGroup``
+/// - ``CommandMenu``
+/// - ``EmptyCommands``
+/// - ``SidebarCommands``
+/// - ``TextEditingCommands``
+/// - ``TextFormattingCommands``
+/// - ``ToolbarCommands``
 @available(iOS 14.0, macOS 11.0, *)
 @available(tvOS, unavailable)
 @available(watchOS, unavailable)
