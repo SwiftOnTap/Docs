8520948f40bb1c3c982fea3c7e7da227c406a356
benjaminsage <benjaminlsage@gmail.com>
2021-03-22 19:52:44 -0700

    Added UnitPoint docs

--- SwiftUI.swift
+++ SwiftUI.swift
@@ -37207,7 +37207,7 @@ extension Text {
     /// ![fontWeight Example 1](https://bananadocs-documentation-assets.s3-us-west-2.amazonaws.com/text-fontWeight-example-1.png)
     ///
     /// - Note: The example above uses [*type inference*](https://www.swiftbysundell.com/basics/type-inference/)
-    /// shorten the code. The full version of using the modifier would
+    /// to shorten the code. The full version of using the modifier would
     /// look like this:
     ///
     /// ```
@@ -40038,24 +40038,34 @@ public struct UIViewRepresentableContext<Representable> where Representable : UI
 
 /// A dynamic data structure for representing a point in a view.
 ///
-/// A `UnitPoint` takes in `x` and `y` coordinates measured in points.
+/// ### What is a `UnitPoint`?
+///
+/// A `UnitPoint` is a 2D coordinate. Its `x` and `y` values are measured
+/// from `0.0` to `1.0`, and represent fractions of the parent view.
+/// The positive **x-axis** runs from **leading** edge to **trailing** edge, and the positve
+/// **y-axis** moves from **top** to **bottom**.
 ///
 /// There are two ways to make a `UnitPoint`:
 /// you can either customize the `x` and `y` coordinates, or you can use a
 /// predefined constant.
 /// SwiftUI has 10 `UnitPoint` constants:
-/// 1. ``UnitPoint/zero``
-/// 2. ``UnitPoint/center``
-/// 3. ``UnitPoint/topleading``
-/// 4. ``UnitPoint/top``
-/// 5. ``UnitPoint/toptrailing``
-/// 6. ``UnitPoint/trailing``
-/// 7. ``UnitPoint/bottomtrailing``
-/// 8. ``UnitPoint/bottom``
-/// 9. ``UnitPoint/bottomleading``
-/// 10. ``UnitPoint/leading``
+/// 1. ``UnitPoint/zero``: `x = 0` and `y = 0`
+/// 2. ``UnitPoint/center``: `x = 0.5` and `y = 0.5`
+/// 3. ``UnitPoint/topleading``: `x = 0` and `y = 0`
+/// 4. ``UnitPoint/top``: `x = 0.5` and `y = 0`
+/// 5. ``UnitPoint/toptrailing``: `x = 1` and `y = 0`
+/// 6. ``UnitPoint/trailing``: `x = 1` and `y = 0.5`
+/// 7. ``UnitPoint/bottomtrailing``: `x = 1` and `y = 1`
+/// 8. ``UnitPoint/bottom``: `x = 0.5` and `y = 1`
+/// 9. ``UnitPoint/bottomleading``: `x = 0` and `y = 1`
+/// 10. ``UnitPoint/leading``: `x = 0` and `y = 0.5`
 ///
-/// `UnitPoint`s are used commonly in shapes and gradients.
+/// ### How to use a `UnitPoint`
+///
+/// `UnitPoints`s can be used to specify the anchor point for view
+/// transformations like ``View/rotationEffect(_:anchor:)``
+/// and ``View/scaleEffect(_:anchor:)-31528``.They are used commonly in
+/// ``Shape``s and gradients.
 ///
 /// For example, a ``LinearGradient`` accepts a `UnitPoint` for the
 /// `startPoint` and `endPoint` parameters.
@@ -40063,14 +40073,15 @@ public struct UIViewRepresentableContext<Representable> where Representable : UI
 /// unit points.
 ///
 /// ```
-/// struct ExampleView: View {
-///     let colors: [Color] = [.yellow, .orange]
-///
+/// struct ContentView: View {
 ///     var body: some View {
-///         Rectangle()
-///             .fill(LinearGradient(gradient: Gradient(colors: colors),
-///                                  startPoint: .top,
-///                                  endPoint: .bottom))
+///         let colors = [Color.yellow, Color.orange]
+///         let gradient = Gradient(colors: colors)
+///         let linearGradient = LinearGradient(gradient: gradient,
+///                                             startPoint: UnitPoint.top,
+///                                             endPoint: UnitPoint.bottom)
+///         return Rectangle()
+///             .fill(linearGradient)
 ///             .frame(width:100, height:100)
 ///     }
 /// }
@@ -40078,19 +40089,166 @@ public struct UIViewRepresentableContext<Representable> where Representable : UI
 ///
 /// ![LinearGradient Example 1](1199EBDE-F3C9-4FA5-900B-98139BD6C0CC.png)
 ///
+/// To use a unit point as an anchor for rotation, pass it to the
+/// ``View/rotationEffect(_:anchor:)`` modifier:
+///
+/// ```
+/// struct ContentView: View {
+///     var body: some View {
+///         VStack(spacing: 200) {
+///             Text("I'm rotated around my center 👈")
+///                 .rotationEffect(.degrees(22), anchor: UnitPoint.center)
+///                 .border(Color.gray)
+///             Text("I'm rotated on the left 🍃")
+///                 .rotationEffect(.degrees(22), anchor: UnitPoint.leading)
+///                 .border(Color.gray)
+///         }
+///     }
+/// }
+/// ```
+///
+/// ![](unitpoint-anchor.png)
+///
+/// You can get the same effect by creating your own custom `UnitPoint`s,
+/// using the ``UnitPoint/init(x:y:)`` initialzer:
+///
+/// ```
+/// struct ContentView: View {
+///     let center = UnitPoint(x: 0.5, y: 0.5)
+///     let leading = UnitPoint(x: 0, y: 0.5)
+///
+///     var body: some View {
+///         VStack(spacing: 200) {
+///             Text("I'm rotated around my center 👈")
+///                 .rotationEffect(.degrees(22), anchor: center)
+///                 .border(Color.gray)
+///             Text("I'm rotated on the left 🍃")
+///                 .rotationEffect(.degrees(22), anchor: leading)
+///                 .border(Color.gray)
+///         }
+///     }
+/// }
+/// ```
+///
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 @frozen public struct UnitPoint : Hashable {
 
 	/// The x coordinate of the point, measured in points.
+    ///
+    /// The `x` value of a ``UnitPoint`` runs from `0.0` to `1.0`,
+    /// and represents the fractional distance of the parent view's
+    /// width. `0` corresponds to the **leading** edge, and `1` corresponds
+    /// to the **trailing** edge.
+    ///
+    /// You can also use values outside this range, like `2.0`, and they'll represent
+    /// places **outside** of the view.
+    ///
+    /// You usually specify this value using the ``UnitPoint``
+    /// ``UnitPoint/init(x:y:)`` initialzer, but you can also specify it
+    /// directly:
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     var body: some View {
+    ///         var point = UnitPoint()
+    ///         point.x = 0.5
+    ///
+    ///         return Text("I'm rotated around my center 👈")
+    ///             .rotationEffect(.degrees(22), anchor: point)
+    ///             .border(Color.gray)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// ![](unitpoint-rotation-center.png)
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     public var x: CGFloat
 
     /// The y coordinate of the point, measured in points.
+    ///
+    /// The `y` value of a ``UnitPoint`` runs from `0.0` to `1.0`,
+    /// and represents the fractional distance of the parent view's
+    /// height. `0` corresponds to the **top** edge, and `1` corresponds
+    /// to the **bottom** edge.
+    ///
+    /// You can also use values outside this range, like `2.0`, and they'll represent
+    /// places **outside** of the view.
+    ///
+    /// You usually specify this value using the ``UnitPoint``
+    /// ``UnitPoint/init(x:y:)`` initialzer, but you can also specify it
+    /// directly:
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     var body: some View {
+    ///         var point = UnitPoint()
+    ///         point.y = 0.5
+    ///         point.x = 0.5
+    ///
+    ///         return Text("I'm rotated around my center 👈")
+    ///             .rotationEffect(.degrees(22), anchor: point)
+    ///             .border(Color.gray)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// ![](unitpoint-rotation-center.png)
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     public var y: CGFloat
 
     /// Creates a unit point with x and y values of 0.
+    ///
+    /// Use this convenience initializer to create a unit point with
+    /// `x` and `y` values of `0`. This corresponds to the top leading
+    /// edge of the view.
+    ///
+    /// This intializer is equivalent to ``UnitPoint``'s ``UnitPoint/zero``
+    /// static constant.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     let point = UnitPoint()
+    ///
+    ///     var body: some View {
+    ///         Text("I'm rotated on the left 🍃")
+    ///             .rotationEffect(.degrees(22), anchor: point)
+    ///             .border(Color.gray)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// ![](unitpoint-rotation-left.png)
     @inlinable public init() { }
 
     /// Creates a unit point from x and y values.
+    ///
+    /// Use this initializer to create a custom ``UnitPoint``.
+    ///
+    /// The `x` and `y` values of a ``UnitPoint`` run from `0.0` to `1.0`,
+    /// and represents the fractional distance of the parent view's
+    /// width. `0` corresponds to the **leading** or **top** edge,
+    /// and `1` corresponds to the **trailing** or **bottom** edge.
+    ///
+    /// You can also use values outside this range, like `2.0`, and they'll represent
+    /// places **outside** of the view.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     let point = UnitPoint(x: 0.5, y: 0.5)
+    ///
+    ///     var body: some View {
+    ///         Text("I'm rotated around my center 👈")
+    ///             .rotationEffect(.degrees(22), anchor: point)
+    ///             .border(Color.gray)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// ![](unitpoint-rotation-center.png)
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     ///
     /// - Parameters:
     ///   x: The x coordinate of the unit point.
@@ -40098,33 +40256,230 @@ public struct UIViewRepresentableContext<Representable> where Representable : UI
     @inlinable public init(x: CGFloat, y: CGFloat) { }
 
     /// A unit point with x and y values of 0.
+    ///
+    /// Use this static constant to specify a ``UnitPoint`` with an
+    /// `x` of `0` and a `y` of `0`.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     var body: some View {
+    ///         Rectangle()
+    ///             .frame(width: 100, height: 100)
+    ///             .rotationEffect(.degrees(22), anchor: .zero)
+    ///             .border(Color.orange)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// ![](unitpoint-zero.png)
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     public static let zero: UnitPoint
 
     /// The unit point at the center of a given view.
+    ///
+    /// Use this static constant to specify a ``UnitPoint`` with an
+    /// `x` of `0.5` and a `y` of `0.5`.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     var body: some View {
+    ///         Rectangle()
+    ///             .frame(width: 100, height: 100)
+    ///             .rotationEffect(.degrees(22), anchor: .center)
+    ///             .border(Color.orange)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// ![](unitpoint-center.png)
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     public static let center: UnitPoint
 
     /// The unit point at the middle of the leading edge of a given view.
+    ///
+    /// Use this static constant to specify a ``UnitPoint`` with an
+    /// `x` of `0` and a `y` of `0.5`.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     @State var angle: Double = 0
+    ///
+    ///     var body: some View {
+    ///         VStack {
+    ///             Slider(value: $angle, in: 0...360)
+    ///             Rectangle()
+    ///                 .frame(width: 100, height: 100)
+    ///                 .rotationEffect(.degrees(22), anchor: .leading)
+    ///                 .border(Color.orange)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     public static let leading: UnitPoint
 
     /// The unit point at the middle of the trailing edge of a given view.
+    ///
+    /// Use this static constant to specify a ``UnitPoint`` with an
+    /// `x` of `1` and a `y` of `0.5`.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     @State var angle: Double = 0
+    ///
+    ///     var body: some View {
+    ///         VStack {
+    ///             Slider(value: $angle, in: 0...360)
+    ///             Rectangle()
+    ///                 .frame(width: 100, height: 100)
+    ///                 .rotationEffect(.degrees(22), anchor: .trailing)
+    ///                 .border(Color.orange)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     public static let trailing: UnitPoint
 
     /// The unit point at the middle of the top edge of a given view.
+    ///
+    /// Use this static constant to specify a ``UnitPoint`` with an
+    /// `x` of `0.5` and a `y` of `0`.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     @State var angle: Double = 0
+    ///
+    ///     var body: some View {
+    ///         VStack {
+    ///             Slider(value: $angle, in: 0...360)
+    ///             Rectangle()
+    ///                 .frame(width: 100, height: 100)
+    ///                 .rotationEffect(.degrees(22), anchor: .top)
+    ///                 .border(Color.orange)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
+    ///
     public static let top: UnitPoint
 
     /// The unit point at the middle of the bottom edge of a given view.
+    ///
+    /// Use this static constant to specify a ``UnitPoint`` with an
+    /// `x` of `0.5` and a `y` of `1`.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     @State var angle: Double = 0
+    ///
+    ///     var body: some View {
+    ///         VStack {
+    ///             Slider(value: $angle, in: 0...360)
+    ///             Rectangle()
+    ///                 .frame(width: 100, height: 100)
+    ///                 .rotationEffect(.degrees(22), anchor: .bottom)
+    ///                 .border(Color.orange)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     public static let bottom: UnitPoint
 
     /// The unit point at the top leading corner of a given view.
+    ///
+    /// Use this static constant to specify a ``UnitPoint`` with an
+    /// `x` of `0` and a `y` of `0`.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     @State var angle: Double = 0
+    ///
+    ///     var body: some View {
+    ///         VStack {
+    ///             Slider(value: $angle, in: 0...360)
+    ///             Rectangle()
+    ///                 .frame(width: 100, height: 100)
+    ///                 .rotationEffect(.degrees(22), anchor: .topLeading)
+    ///                 .border(Color.orange)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     public static let topLeading: UnitPoint
 
     /// The unit point at the top trailing corner of a given view.
+    ///
+    /// Use this static constant to specify a ``UnitPoint`` with an
+    /// `x` of `1` and a `y` of `0`.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     @State var angle: Double = 0
+    ///
+    ///     var body: some View {
+    ///         VStack {
+    ///             Slider(value: $angle, in: 0...360)
+    ///             Rectangle()
+    ///                 .frame(width: 100, height: 100)
+    ///                 .rotationEffect(.degrees(22), anchor: .topTrailing)
+    ///                 .border(Color.orange)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     public static let topTrailing: UnitPoint
 
     /// The unit point at the bottom leading corner of a given view.
+    ///
+    /// Use this static constant to specify a ``UnitPoint`` with an
+    /// `x` of `0` and a `y` of `1`.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     @State var angle: Double = 0
+    ///
+    ///     var body: some View {
+    ///         VStack {
+    ///             Slider(value: $angle, in: 0...360)
+    ///             Rectangle()
+    ///                 .frame(width: 100, height: 100)
+    ///                 .rotationEffect(.degrees(22), anchor: .bottomLeading)
+    ///                 .border(Color.orange)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     public static let bottomLeading: UnitPoint
 
     /// The unit point at the bottom trailing corner of a given view.
+    ///
+    /// Use this static constant to specify a ``UnitPoint`` with an
+    /// `x` of `1` and a `y` of `1`.
+    ///
+    /// ```
+    /// struct ContentView: View {
+    ///     @State var angle: Double = 0
+    ///
+    ///     var body: some View {
+    ///         VStack {
+    ///             Slider(value: $angle, in: 0...360)
+    ///             Rectangle()
+    ///                 .frame(width: 100, height: 100)
+    ///                 .rotationEffect(.degrees(22), anchor: .bottomTrailing)
+    ///                 .border(Color.orange)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// See ``UnitPoint`` for more on how and when to use unit points.
     public static let bottomTrailing: UnitPoint
 
     /// The hash value.
