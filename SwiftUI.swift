cadf459768af78ed1133074f0639521f13ae836e
elizcordo <79473811+elizcordo@users.noreply.github.com>
2021-04-14 17:37:48 -0700

    GBS modifier doc draft

--- SwiftUI.swift
+++ SwiftUI.swift
@@ -51815,13 +51815,26 @@ extension View {
     ///
     /// Use this method to change the style of your ``GroupBox``.
     ///
-    /// There is currently one pre-built style, called
-    /// ``DefaultGroupBoxStyle``, and the protocol
-    /// ``GroupBoxStyle`` lets you creat your own custom styles.
+    /// There is currently only one pre-built style, called ``DefaultGroupBoxStyle``:
     ///
-    /// See ``GroupBoxStyle`` for info on creating custom group box styles.
+    /// ```
+    /// struct ContentView: View {
+    ///     var body: some View {
+    ///         GroupBox(label: Label("Default GroupBox", systemImage: "smiley"))
+    ///         {
+    ///             Text("GroupBox content!")
+    ///         }
+    ///         .padding()
+    ///         .groupBoxStyle(DefaultGroupBoxStyle())
+    ///     }
+    /// }
+    /// ```
     ///
+    /// ![A view containing a GroupBox labeled "Default GroupBox" coupled with a
+    /// smiley face system image and a text view within. Padding and the
+    /// DefaultGroupBoxStyle have been applied to the GroupBox.](group-box-style-ex.png)
     ///
+    /// The protocol ``GroupBoxStyle`` lets you create your own custom styles.
     ///
     /// - Parameter style: The style to apply to boxes within this view.
     public func groupBoxStyle<S>(_ style: S) -> some View where S : GroupBoxStyle { }
